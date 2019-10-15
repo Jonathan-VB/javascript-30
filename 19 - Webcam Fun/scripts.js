8520f3e6e2b96323aa88660ad7cef5c89343b996
Jonathan Dempsey <jonathanedempsey@gmail.com>
2019-10-15 12:41:16 +0100

    19 - How to get the camera working correctly
    Webcam wont work over a non-secure browser so we either need https:// or localhost. Use '$ npm install' to install browsersync then use '$ npm start' to run it (over localhost).

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -1,3 +1,8 @@
+/**
+ * Webcam wont work over a non-secure browser so we either need https:// or localhost.
+ * Use '$ npm install' to install browsersync then use '$ npm start' to run it (over localhost).
+ */
+
 const video = document.querySelector('.player');
 const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
