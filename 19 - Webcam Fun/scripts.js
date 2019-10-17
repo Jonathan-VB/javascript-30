27aef935b8c7a8d16e024a7a16c430746db97b2f
Jonathan Dempsey <jonathanedempsey@gmail.com>
2019-10-17 18:34:42 +0100

    19 - Create clickable image to download snapshot

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -44,7 +44,7 @@ function takePhoto() {
   const link = document.createElement('a');
   link.href = data;
   link.setAttribute('download', 'handsome');
-  link.textContent = 'Download Image';
+  link.innerHTML = `<img src="${data}" alt="Handsome Dude" />`;
   strip.insertBefore(link, strip.firstChild);
   console.log(data);
 }
