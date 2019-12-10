f79ccdc219cfec87e3a4668c6bd433c5aab810c6
Jonathan Dempsey <jonathanedempsey@gmail.com>
2019-12-10 12:36:00 +0000

    29 - Ironed out some timing bugs

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -1,11 +1,28 @@
-function timer(seconds) {
-  const now = Date.now();
-  const then = now + seconds * 1000;
+// Global countdown variable
+let countdown;
 
-  setInterval(() => {
-    const secondsLeft = (then - Date.now()) / 1000;
-    console.log(secondsLeft);
+function timer(seconds) {
+  const now = Date.now(); // When the timer started - in milliseconds
+  const then = now + seconds * 1000; // When it stops - Now plus the amount of seconds you want the timer to run
+  displayTimeLeft(seconds);
+
+  countdown = setInterval(() => {
+    const secondsLeft = Math.round((then - Date.now()) / 1000); // in milliseconds so divide by 1000. Round to nearest whole number
+
+    // check if we should stop it
+    if(secondsLeft < 0) {
+      clearInterval(countdown);
+      return;
+    }
+
+    // display it
+    displayTimeLeft(secondsLeft);
   }, 1000);
 }
 
-timer(100);
+// Display time left
+function displayTimeLeft(seconds) {
+  console.log(seconds);
+}
+
+timer(10);
